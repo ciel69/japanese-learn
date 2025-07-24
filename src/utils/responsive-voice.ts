f1a120cab6dfd891ef22bf21b061dace4b772584
Bukhaltsov Alexei <alex453542026@yandex.ru>
2025-07-24 09:29:53 +0300

    доработка озвучки для разных браузеров

--- src/utils/responsive-voice.ts
+++ src/utils/responsive-voice.ts
@@ -1,5 +1,8 @@
 declare global {
   interface Window {
+    /**
+     * Типизация сторонней библиотеки responsivevoice.js (https://responsivevoice.org/api/)
+     */
     responsiveVoice: {
       speak: (text: string, voice?: string, params?: never) => void
       // ... другие методы
@@ -8,13 +11,30 @@ declare global {
 }
 
 export const useResponsiveVoice = () => {
+  const responsiveVoice = (text: string) => {
+    try {
+      window.responsiveVoice?.speak(text, 'Japanese Female')
+    } catch (error) {
+      console.error('Ошибка синтеза речи:', error)
+    }
+  }
+
+  const speak = (text: string) => {
+    // Если нет нативной поддержки, вызываем функционал библиотеки
+    if (!window.speechSynthesis) {
+      responsiveVoice(text)
+      return
+    }
+
+    const utterance = new SpeechSynthesisUtterance(text)
+    utterance.lang = 'ja-JP'
+    utterance.rate = 1.0
+    utterance.pitch = 1.0
+
+    speechSynthesis.speak(utterance)
+  }
+
   return {
-    speak: (text: string) => {
-      try {
-        window.responsiveVoice?.speak(text, 'Japanese Female')
-      } catch (error) {
-        console.error('Ошибка синтеза речи:', error)
-      }
-    },
+    speak,
   }
 }
