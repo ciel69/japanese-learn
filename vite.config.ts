0923b1613322be2c26bc288d01d9dfc9f99b8c12
Bukhaltsov Alexei <alex453542026@yandex.ru>
2025-07-29 11:59:34 +0300

    добавлена поддержка PWA

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,23 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-console.log('process.env.NODE_ENV', process.env.NODE_ENV)
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/japanese-learn/' : '/',
-  plugins: [vue(), vueJsx(), tailwindcss(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+    }),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
