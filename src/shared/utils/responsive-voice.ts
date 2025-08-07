58cd4a7221d0ab34656296a5116f6d1a662a8d63
Bukhaltsov Alexei <alex453542026@yandex.ru>
2025-08-07 12:53:05 +0300

    добавлена возможность использования локального сервера, используется локальный сервер озвучки

--- src/shared/utils/responsive-voice.ts
+++ src/shared/utils/responsive-voice.ts
@@ -1,37 +1,49 @@
-declare global {
-  interface Window {
-    /**
-     * Типизация сторонней библиотеки responsivevoice.js (https://responsivevoice.org/api/)
-     */
-    responsiveVoice: {
-      speak: (text: string, voice?: string, params?: never) => void
-      // ... другие методы
-    }
-  }
-}
+import axios from 'axios'
+
+const BASE_URL = 'https://94ccfa-85-174-192-193.ru.tuna.am'
 
 export const useResponsiveVoice = () => {
-  const responsiveVoice = (text: string) => {
+  const generateVoice = async (text: string) => {
     try {
-      window.responsiveVoice?.speak(text, 'Japanese Female')
+      const response = await axios.get(`${BASE_URL}/tts/generate`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*', // или конкретный домен
+          'tuna-skip-browser-warning': 'test-api',
+        },
+        params: {
+          text: text,
+          speaker: 44, // 44 неплох для каны
+          volume: 5,
+          speed: 1.1,
+        },
+        responseType: 'arraybuffer', // Получаем как ArrayBuffer
+      })
+
+      const audioContext = new AudioContext()
+      const buffer = await audioContext.decodeAudioData(response.data)
+
+      const source = audioContext.createBufferSource()
+      source.buffer = buffer
+      source.connect(audioContext.destination)
+      source.start()
     } catch (error) {
-      console.error('Ошибка синтеза речи:', error)
+      console.error('Ошибка генерации голоса:', error)
+      return new Error()
     }
   }
 
-  const speak = (text: string) => {
-    // Если нет нативной поддержки, вызываем функционал библиотеки
-    if (!window.speechSynthesis) {
-      responsiveVoice(text)
-      return
+  const speak = async (text: string) => {
+    try {
+      await generateVoice(text)
+    } catch (e: any) {
+      const utterance = new SpeechSynthesisUtterance(text)
+      utterance.lang = 'ja-JP'
+      utterance.rate = 1.0
+      utterance.pitch = 1.0
+
+      speechSynthesis.speak(utterance)
     }
-
-    const utterance = new SpeechSynthesisUtterance(text)
-    utterance.lang = 'ja-JP'
-    utterance.rate = 1.0
-    utterance.pitch = 1.0
-
-    speechSynthesis.speak(utterance)
   }
 
   return {
